8384a35d9163f70ca53afc1d8dcec9b92de95736
Connor Clancy <clancy.mu@gmail.com>
2024-03-27 21:02:20 -0400

    Add A5 - 3.2

--- Lab5.js
+++ Lab5.js
@@ -45,7 +45,55 @@ const Lab5 = (app) => {
         // convert to string otherwise browser interprets
         // as a status code
         res.send(result.toString());
-      });
+    });
+
+    // 3.2.1 - Objects
+    const assignment = {
+        id: 1, 
+        title: "NodeJS Assignment",
+        description: "Create a NodeJS server with ExpressJS",
+        due: "2021-10-10", 
+        completed: false, 
+        score: 0,
+    };
+
+    app.get("/a5/assignment", (req, res) => {
+        res.json(assignment);
+    });
+
+    // 3.2.2 - Object Properties
+    app.get("/a5/assignment/title", (req, res) => {
+        res.json(assignment.title);
+    });
+    
+    // 3.2.3 Modifying Objects
+    app.get("/a5/assignment/title/:newTitle", (req, res) => {
+        const { newTitle } = req.params;
+        assignment.title = newTitle;
+        res.json(assignment);
+    });
+
+    // 3.2.4 - Module Object 
+    const module = {
+        id: 0,
+        name: "My First Object",
+        description: "Create an object with NodeJS",
+        course: "CS5610"
+    } 
+    
+    app.get("/a5/module", (req, res) => {
+        res.json(module);
+    });
+
+    app.get("/a5/module/name", (req, res) => {
+        res.json(module.name);
+    });
+
+    app.get("/a5/module/name/:newName", (req, res) => {
+        const { newName } = req.params;
+        module.name = newName;
+        res.json(module);
+    });
     
 };
   
