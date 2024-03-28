e28f00ce37366e415a99212fa2d7daf610905869
Connor Clancy <clancy.mu@gmail.com>
2024-03-28 17:11:18 -0400

    Add a5-3.3

--- Lab5.js
+++ Lab5.js
@@ -95,6 +95,102 @@ const Lab5 = (app) => {
         res.json(module);
     });
     
+    // 3.3.1 - Retrieving Arrays
+    const todos = [
+        { id: 1, title: "Task 1", completed: false },
+        { id: 2, title: "Task 2", completed: true },
+        { id: 3, title: "Task 3", completed: false },
+        { id: 4, title: "Task 4", completed: true },
+    ];
+    
+    app.get("/a5/todos", (req, res) => {
+
+        // 3.3.3 - Filtering array items using a query string
+        const { completed } = req.query;
+
+        if (completed !== undefined) {
+          const completedBool = completed === "true";
+          const completedTodos = todos.filter(
+            (t) => t.completed === completedBool);
+          res.json(completedTodos);
+          return;
+        }
+
+        res.json(todos);
+    });
+
+    // 3.3.4 - Creating new Items in an Array 
+    // must be implemented before /:id route (3.2.2) otherwise
+    // the :id path parameter would interoret the create as an ID
+    app.get("/a5/todos/create", (req, res) => {
+        const newTodo = {
+          id: new Date().getTime(),
+          title: "New Task",
+          completed: false,
+        };
+        todos.push(newTodo);
+        res.json(todos);
+      });    
+    
+    // 3.3.2 - Retrieving an Item from an Array by ID
+    app.get("/a5/todos/:id", (req, res) => {
+        const { id } = req.params;
+        const todo = todos.find((t) => t.id === parseInt(id));
+        res.json(todo);
+    });
+
+    // 3.3.5 - Deleting an Item from an Array
+    app.get("/a5/todos/:id/delete", (req, res) => {
+        const { id } = req.params;
+
+        // create a copy of the object to delete by matching the ID
+        const todo = todos.find((t) => t.id === parseInt(id));
+
+        // use indexOf to find the todo's index in the list 
+        const todoIndex = todos.indexOf(todo);
+
+        // splice to remove todo at todoIndex; remove only 1 item
+        if (todoIndex !== -1) {
+          todos.splice(todoIndex, 1);
+        }
+
+        // return the todos list
+        res.json(todos);
+    });
+    
+    // 3.3.6 - Updating at item in an array
+    app.get("/a5/todos/:id/title/:title", (req, res) => {
+        const { id, title } = req.params;
+        const todo = todos.find((t) => t.id === parseInt(id));
+        todo.title = title;
+        res.json(todos);
+    });
+
+    // 3.3.7a - Mark an item completed
+    app.get("/a5/todos/:id/completed/:completed", (req, res) => {
+        const { id, completed } = req.params;
+
+        // create a copy of the object to delete by matching the ID
+        const todo = todos.find((t) => t.id === parseInt(id));
+
+        // Convert the string parameter to a boolean value
+        const isCompleted = completed === "true"; 
+
+        // Update the completed flag with the boolean value
+        todo.completed = isCompleted;
+
+
+        // return the todos list
+        res.json(todos);
+    });
+    
+    // 3.3.7b - Update the description
+    app.get("/a5/todos/:id/description/:description", (req, res) => {
+        const { id, description } = req.params;
+        const todo = todos.find((t) => t.id === parseInt(id));
+        todo.description = description;
+        res.json(todos);
+    });
 };
   
 export default Lab5;
